59e80b65e582d3d4f012ccb63cc1e7b05ca607fc
Phil McCarthy <philmcc2631@gmail.com>
2021-02-11 16:07:43 -0700

    Add AR associations to models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :invoices
+  has_many :merchants, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
   
   enum status: { pending: 0, packed: 1, shipped: 2 }
 end
