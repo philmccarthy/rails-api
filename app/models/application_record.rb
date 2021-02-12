d267459efa1ef5fb0098f4c1d5a3069729285ede
Phil McCarthy <philmcc2631@gmail.com>
2021-02-11 22:19:33 -0700

    Abstract default retrieve method to AppRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.retrieve(results_per_page, page_number)
+    if page_number == 1
+      limit(results_per_page)
+    else
+      limit(results_per_page).offset(results_per_page * (page_number - 1))
+    end
+  end
 end
