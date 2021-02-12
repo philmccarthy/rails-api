8def5361f30b03c27f1a483c338917981308d6b4
Phil McCarthy <philmcc2631@gmail.com>
2021-02-11 22:07:05 -0700

    Add initial factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,5 +1,20 @@
-# FactoryBot.define do
-  # factory :merchant do
-    # name { Faker::Company.name }
+FactoryBot.define do
+  factory :merchant do
+    name { Faker::Company.name }
+  end
+
+  factory :customer do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+  end
+
+  factory :item do
+    name { Faker::Food.spice }
+    description { Faker::Food.description }
+    unit_price { Faker::Number.decimal(l_digits: 2) }
+  end
+
+  # factory :invoice do
+  #   status { [:pending ] }
   # end
-# end
+end
