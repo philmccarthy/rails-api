30870debc4f8f49f3a6755bdec6e818bf4485cc6
Phil McCarthy <philmcc2631@gmail.com>
2021-02-11 22:10:49 -0700

    Add Merchant model retrieve method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,3 +3,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   has_one :merchant, through: :item
 end
+
+
+# Order with highest potential revenue
+#   InvoiceItem.select('quantity * unit_price AS potential_revenue').where(invoice_id: 4844)
