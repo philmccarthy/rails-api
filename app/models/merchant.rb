4bc512a9e5dfd8fdf5d675fea3221e5d3cc7f71a
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 13:54:49 -0700

    Abstract retrieve method to AppRecord

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,12 +3,4 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
-
-  def self.retrieve(results_per_page, page_number)
-    if page_number == 1
-      limit(results_per_page)
-    else
-      limit(results_per_page).offset(results_per_page * (page_number - 1))
-    end
-  end
 end
