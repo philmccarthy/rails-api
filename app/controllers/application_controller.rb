1c554d79c867b6e6339cec8c673d65e2d9bbf0b1
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 13:54:23 -0700

    Revise params flow control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
   def results_per_page
-    return params[:per_page].to_i if params[:per_page]
+    return params[:per_page].to_i if params[:per_page].to_i > 0
     20
   end
 
   def page_number
-    return params[:page].to_i if params[:page]
+    return params[:page].to_i if params[:page].to_i >= 1
     1
   end
 end
