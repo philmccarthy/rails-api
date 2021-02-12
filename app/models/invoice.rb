cba711321df2b4cf334e3fa29cbaa56e67822e66
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 11:52:00 -0700

    Undo invoices.status migrations to create an enum, stick with strings!

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,4 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  
-  enum status: { pending: 0, packaged: 1, shipped: 2, returned: 3 }
 end
