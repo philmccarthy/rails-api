6dd2b73ca451827a0e2542790503a2d3933573b3
Phil McCarthy <philmcc2631@gmail.com>
2021-02-13 16:35:40 -0700

    Add invoice item test and callback to delete a newly empty invoice

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,12 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   has_one :merchant, through: :item
+
+  after_destroy :destroy_empty_invoice
+
+  def destroy_empty_invoice
+    invoice.destroy if !invoice.invoice_items.any?
+  end
 end
 
 
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,6 +11,36 @@ RSpec.describe InvoiceItem, type: :model do
     it { should have_one(:merchant).through(:item) }
   end
 
-  # describe 'instance methods' do
-  # end
+  describe 'instance methods' do
+    it '#destroy_empty_invoice' do
+      item_1 = create(:item_with_invoices)
+      
+      expect(Invoice.count).to eq(5)
+      
+      all_invoices_have_just_item_1 = item_1.invoices.all? do |invoice|
+        invoice.items.all? do |item|
+          item == item_1
+        end
+      end
+      
+      expect(all_invoices_have_just_item_1).to be true
+      
+      item_2 = create(:item)
+      invoice_2 = create(:invoice)
+      invoice_2.invoice_items.create!(item: item_1, invoice: invoice_2)
+      invoice_2.invoice_items.create!(item: item_2, invoice: invoice_2)
+      
+      all_invoices_have_just_item_1 = Invoice.all.all? do |invoice|
+        invoice.items.all? do |item|
+          item == item_1
+        end
+      end
+      
+      expect(all_invoices_have_just_item_1).to be false
+      
+      item_1.destroy
+
+      expect(Invoice.count).to eq(1)
+    end
+  end
 end
