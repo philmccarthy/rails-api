d37c6710fb04a26d777743f4b2f9a1f7b7009295
Phil McCarthy <philmcc2631@gmail.com>
2021-02-18 20:59:48 -0700

    Add invoice model test for unshipped potential revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:items).through(:invoice_items) }
   end
 
-  # describe 'instance methods' do
-  # end
+  describe 'class methods' do
+    it '::unshipped_potential_revenue' do
+      invoice = create(:invoice, status: 'packaged')
+      create(:invoice_item, invoice: invoice, quantity: 1, unit_price: 100.00)
+
+      unshipped_invoice_with_revenue = Invoice.unshipped_potential_revenue(1)
+
+      expect(unshipped_invoice_with_revenue).to be_an ActiveRecord::Relation
+      expect(unshipped_invoice_with_revenue.first.id).to eq(invoice.id)
+      expect(unshipped_invoice_with_revenue.first.potential_revenue).to eq(100)
+    end
+  end
 end
