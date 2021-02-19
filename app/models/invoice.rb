d718e5bb60cc9cf8bfc18903365223c3e6e41fae
Phil McCarthy <philmcc2631@gmail.com>
2021-02-18 21:00:03 -0700

    Implement AR query on Invoice model for unshipped potential revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,12 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
+
+  class << self
+    def unshipped_potential_revenue(quantity)
+      Invoice.joins(:invoice_items).
+      where(status: 'packaged').
+      select('invoices.*, (invoice_items.quantity * invoice_items.unit_price) AS potential_revenue')
+    end
+  end
 end
